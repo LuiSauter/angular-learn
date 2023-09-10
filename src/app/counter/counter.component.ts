d795a3c40cc0cbc03801cfb03b728b2c03cf1d09
Luis Gabriel Janco <janco7249@gmail.com>
2023-09-10 10:17:34 -0400

    feat: add counter component

---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  template: `
+    <h2>Hello from counter component</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima,
+      dignissimos! Nemo alias ipsum suscipit, impedit porro, quisquam excepturi
+      ducimus beatae necessitatibus itaque consectetur cupiditate ipsa ad quo
+      quasi similique temporibus!
+    </p>
+  `,
+})
+export class CounterComponent {
+  constructor() {}
+}
