0e7892025a0e0095bf36857ad377b961d8f73ab3
Luis Gabriel Janco <janco7249@gmail.com>
2023-09-10 10:18:05 -0400

    feat: add app components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'bases';
+  public title: string = 'Course of Angular';
+  public counter: number = 10;
+
+  increaseBy(value: number): void {
+    this.counter += value;
+  }
+
+  decreaseBy(value: number): void {
+    this.counter -= value;
+  }
+
+  resetCounter(): void {
+    this.counter = 0;
+  }
 }
